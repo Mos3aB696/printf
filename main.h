97f3b84a4f829ea9de7bff36f41d27137d8b4f79
Mos3aB696 <mos3ab696@gmail.com>
2023-08-22 00:42:21 +0300

    final result

--- main.h
+++ main.h
@@ -5,12 +5,10 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-
 /**
  * struct format - match the conversion specifires for printf
  * @c: type char pointer of the specifier
  * @f: type pointer to function
- *
  */
 
 typedef struct format
@@ -29,10 +27,4 @@ int print_int(va_list arg);
 int _abs(int num);
 int print_dec(va_list val);
 int print_percentage(void);
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> f32cc00 (adding library)
 #endif
